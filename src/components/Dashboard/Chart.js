98d731fa3030dcff558cfa3b48ad404bcf9c7b09
MdMuzahid07 <mdmuzahid7396@gmail.com>
2022-09-11 16:22:35 +0600

    react rechart install

--- src/components/Dashboard/Chart.js
+++ src/components/Dashboard/Chart.js
@@ -3,8 +3,7 @@ import React from 'react';
 const Chart = () => {
     return (
         <div>
-            <h1>weather chart</h1>
-            <h2>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsa vitae, deleniti architecto laboriosam doloribus voluptatibus eaque nihil illum cupiditate distinctio.</h2>
+
         </div>
     );
 };
