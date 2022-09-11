4fb4b25de973101d1195b5306e0bdc0920985190
MdMuzahid07 <mdmuzahid7396@gmail.com>
2022-09-11 21:32:28 +0600

    working on weather details card

--- src/components/Dashboard/WeatherDetails/TodayWeatherDetails.js
+++ src/components/Dashboard/WeatherDetails/TodayWeatherDetails.js
@@ -9,7 +9,12 @@ const TodayWeatherDetails = () => {
                         <p>Humidity</p>
                         <svg className='bg-success rounded' xmlns="http://www.w3.org/2000/svg" height="20" width="20"><path d="M10.583 15.458q.167-.02.292-.156.125-.135.125-.344 0-.229-.167-.375-.166-.145-.395-.104-1.209.167-2.219-.562-1.011-.729-1.198-1.979-.021-.188-.156-.313-.136-.125-.323-.125-.23 0-.386.167-.156.166-.114.395.229 1.626 1.541 2.626 1.313 1 3 .77ZM10 18q-2.708 0-4.604-1.896T3.5 11.5q0-2.021 1.552-4.271T10 2q3.458 3.125 4.979 5.354T16.5 11.5q0 2.708-1.896 4.604T10 18Zm0-1.5q2.083 0 3.542-1.458Q15 13.583 15 11.5q0-1.417-1.125-3.104Q12.75 6.708 10 3.979 7.25 6.708 6.125 8.396 5 10.083 5 11.5q0 2.083 1.458 3.542Q7.917 16.5 10 16.5Zm0-6.5Z" fill="#ffffff" /></svg>
                     </div>
-                    <h2 className='text-center font-bold my-2'><span>82</span>%<span>Bad</span></h2>
+                    <h2 className='text-center font-bold my-3'><span>82</span>%<span>Bad</span></h2>
+                    <div className="w-full flex justify-between text-xs px-2">
+                        <span>Good</span>
+                        <span>Normal</span>
+                        <span>bad</span>
+                    </div>
                     <progress className="progress progress-success" value="82" max="100"></progress>
                 </div>
             </div>
