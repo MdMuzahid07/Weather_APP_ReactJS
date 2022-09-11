f6354581f2b55d28c2776f99dc169c116e99e101
MdMuzahid07 <mdmuzahid7396@gmail.com>
2022-09-11 14:33:27 +0600

    working on it

---
+++ src/components/Dashboard/Chart.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Chart = () => {
+    return (
+        <div>
+            <h1>weather chart</h1>
+            <h2>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsa vitae, deleniti architecto laboriosam doloribus voluptatibus eaque nihil illum cupiditate distinctio.</h2>
+        </div>
+    );
+};
+
+export default Chart;
---
+++ src/components/Dashboard/WeatherDetails/TodayWeatherDetails.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const TodayWeatherDetails = () => {
+    return (
+        <div>
+            <h2>Weather details</h2>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Earum minima esse ipsam distinctio qui magnam provident, obcaecati reiciendis cumque repudiandae, moficia beatae, nostrum possimus optio blanditiis amet itaque aliquid non maxime consequuntur aliquam inventore temporibus! Eius provident perspiciatis aut dolor, vero vitae unde tempore! Ipsa modi autem architecto libero, minima tenetur optio quasi ad!</p>
+        </div>
+    );
+};
+
+export default TodayWeatherDetails;
